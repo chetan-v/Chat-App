4e28bd8d9de211e002c0d7694e30ea6afe36d2ce
Chetan <63091769+chetan-v@users.noreply.github.com>
2024-04-12 16:05:08 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "password",
-  host: "my-db.czhyu20vbehy.ap-south-1.rds.amazonaws.com",
-  port: 5432,
-  database: "initial_db",
+  password:"password",
+  host:"localhost",
+  port:5432,
+  database:"dynamic_chat"
 });
 
 module.exports = pool;
